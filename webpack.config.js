1aa06f98af029d145abd1b6bed96b9d65f9daca6
aliaksei-lithium <aliaksei.zhynhiarouski@gmail.com>
2016-05-31 10:24:20 +0300

    Updates in design.

--- webpack.config.js
+++ webpack.config.js
@@ -19,21 +19,23 @@ var webpackConfig = {
                 loader: ExtractTextPlugin.extract("style-loader", "css-loader!less-loader")
             },
             {
+                test: /\.html$/,
+                exclude: /index\.html$/, // you need to exclude your base template (unless you do not want this plugin own templating feature)
+                loader: "html"
+            },
+            {
                 test: /\.(jpe?g|png|gif|svg)$/i,
                 loaders: [
                     'file?hash=sha512&digest=hex&name=[hash].[ext]',
                     'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
                 ]
-            },
-            {
-                test: /\.html$/,
-                loader: 'html-loader'
             }
         ]
     },
     plugins: [
         new ExtractTextPlugin("[name].css"),
         new HtmlWebpackPlugin({
+            inject: true,
             filename: 'index.html',
             template: 'src/index.html',
             title: "Java Professionals Community"
